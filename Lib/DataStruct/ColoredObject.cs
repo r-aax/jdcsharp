a02e3195c7a14132c34388020749fa77b237ae2b
Joy Developing <joydeveloping@gmail.com>
2014-12-23 17:27:37 +0300

    Lib : ColoredObject and ColoresManager for DataStruct

---
+++ Lib/DataStruct/ColoredObject.cs
@@ -0,0 +1,88 @@
+﻿// Copyright Joy Developing.
+
+namespace Lib.DataStruct
+{
+    /// <summary>
+    /// Object that can has color (set of colors).
+    /// </summary>
+    public class ColoredObject
+    {
+        /// <summary>
+        /// Mask of colors set.
+        /// </summary>
+        private int ColoresMask;
+
+        /// <summary>
+        /// Initialization.
+        /// </summary>
+        public ColoredObject()
+        {
+            ClearColores();
+        }
+
+        /// <summary>
+        /// Color mask.
+        /// </summary>
+        /// <param name="n">color number</param>
+        /// <returns>color mask</returns>
+        static int ColorMask(int n)
+        {
+            return 1 << n;
+        }
+
+        /// <summary>
+        /// Check if color is set.
+        /// </summary>
+        /// <param name="n">color number</param>
+        /// <returns>true - if color is set, false - in another case</returns>
+        public bool IsColored(int n)
+        {
+            return (ColoresMask & ColorMask(n)) != 0;
+        }
+
+        /// <summary>
+        /// Check if color is not set.
+        /// </summary>
+        /// <param name="n">color number</param>
+        /// <returns>true - if color is not set, false - in another case</returns>
+        public bool IsNotColored(int n)
+        {
+            return !IsColored(n);
+        }
+
+        /// <summary>
+        /// Set color.
+        /// </summary>
+        /// <param name="n">color number</param>
+        public void SetColor(int n)
+        {
+            ColoresMask |= ColorMask(n);
+        }
+
+        /// <summary>
+        /// Clear color.
+        /// </summary>
+        /// <param name="n">color number</param>
+        public void ClearColor(int n)
+        {
+            ColoresMask &= ~ColorMask(n);
+        }
+
+        /// <summary>
+        /// Switch color.
+        /// </summary>
+        /// <param name="n">color number</param>
+        public void SwitchColor(int n)
+        {
+            ColoresMask ^= ColorMask(n);
+        }
+
+        /// <summary>
+        /// Clear all colors.
+        /// </summary>
+        public void ClearColores()
+        {
+            ColoresMask = 0;
+        }
+    }
+}
---
+++ Lib/DataStruct/ColoresManager.cs
@@ -0,0 +1,56 @@
+﻿// Copyright Joy Developing.
+
+namespace Lib.DataStruct
+{
+    /// <summary>
+    /// Colors manager.
+    /// </summary>
+    public class ColorsManager : ColoredObject
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ColorsManager()
+        {
+            ClearColores();
+        }
+
+        /// <summary>
+        /// Clear all colors.
+        /// </summary>
+        public void FreeColors()
+        {
+            ClearColores();
+        }
+
+        /// <summary>
+        /// New color allocation.
+        /// </summary>
+        /// <returns>new color</returns>
+        public int AllocColor()
+        {
+            int len = sizeof(int) * 8;
+
+            for (int i = 0; i < len; i++)
+            {
+                if (IsNotColored(i))
+                {
+                    SetColor(i);
+
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Free color.
+        /// </summary>
+        /// <param name="n">color number</param>
+        public void FreeColor(int n)
+        {
+            ClearColor(n);
+        }
+    }
+}
