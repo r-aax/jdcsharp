12a835e457c42e6429fc010ed3918f0bafedeb7a
Joy Developing <joydeveloping@gmail.com>
2014-12-24 17:41:35 +0300

    Lib : some Net functions

---
+++ Lib/Net/UDP.cs
@@ -0,0 +1,72 @@
+﻿// Copyright Joy Developing.
+
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lib.Net
+{
+    /// <summary>
+    /// <c>UDP</c> functionality.
+    /// </summary>
+    public static class UDP
+    {
+        /// <summary>
+        /// Create end point.
+        /// </summary> 
+        /// <param name="addr">address</param>
+        /// <param name="port">port</param>
+        /// <returns>end point</returns>
+        public static IPEndPoint EndPoint(string addr, int port)
+        {
+            IPAddress ip = IPAddress.Parse(addr);
+
+            return new IPEndPoint(ip, port);
+        }
+
+        /// <summary>
+        /// Send bytes array with <c>UDP</c>.
+        /// </summary>
+        /// <param name="bytes">bytes array</param>
+        /// <param name="end_point">end point</param>
+        public static void SendTo(byte[] bytes, IPEndPoint end_point)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            s.SendTo(bytes, end_point);
+            s.Close();
+        }
+
+        /// <summary>
+        /// Send bytes array with <c>UDP</c>.
+        /// </summary>
+        /// <param name="bytes">bytes array</param>
+        /// <param name="addr">address</param>
+        /// <param name="port">port</param>
+        public static void SendTo(byte[] bytes, string addr, int port)
+        {
+            SendTo(bytes, EndPoint(addr, port));
+        }
+
+        /// <summary>
+        /// Send string with <c>UDP</c>.
+        /// </summary>
+        /// <param name="str">string</param>
+        /// <param name="end_point">end point</param>
+        public static void SendTo(string str, IPEndPoint end_point)
+        {
+            SendTo(Encoding.ASCII.GetBytes(str), end_point);
+        }
+
+        /// <summary>
+        /// Send string with <c>UDP</c>.
+        /// </summary>
+        /// <param name="str">string</param>
+        /// <param name="addr">address</param>
+        /// <param name="port">port</param>
+        public static void SendTo(string str, string addr, int port)
+        {
+            SendTo(Encoding.ASCII.GetBytes(str), EndPoint(addr, port));
+        }
+    }
+}
---
+++ Lib/Net/UDPListener.cs
@@ -0,0 +1,104 @@
+﻿// Copyright Joy Developing.
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lib.Net
+{
+    /// <summary>
+    /// <c>UDP</c> listener.
+    /// </summary>
+    public class UDPListener
+    {
+        /// <summary>
+        /// Port.
+        /// </summary>
+        private int Port = 0;
+
+        /// <summary>
+        /// End point.
+        /// </summary>
+        private IPEndPoint EndPoint = null;
+
+        /// <summary>
+        /// <c>UDP</c> client.
+        /// </summary>
+        private UdpClient UDPClient = null;
+
+        /// <summary>
+        /// Receive bytes handler.
+        /// </summary>
+        /// <param name="bytes">массив байтов</param>
+        public delegate void OnReceiveBytesHandler(byte[] bytes);
+
+        /// <summary>
+        /// Receive bytes event.
+        /// </summary>
+        public event OnReceiveBytesHandler OnReceiveBytes = null;
+
+        /// <summary>
+        /// Receive string handler.
+        /// </summary>
+        /// <param name="str">строка</param>
+        public delegate void OnReceiveStringHandler(string str);
+
+        /// <summary>
+        /// Receive string event.
+        /// </summary>
+        public event OnReceiveStringHandler OnReceiveString = null;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="port">port</param>
+        public UDPListener(int port)
+        {
+            Port = port;
+            EndPoint = new IPEndPoint(IPAddress.Any, Port);
+            UDPClient = new UdpClient(EndPoint);
+            BeginReceive();
+        }
+
+        /// <summary>
+        /// Begin receive.
+        /// </summary>
+        private void BeginReceive()
+        {
+            UDPClient.BeginReceive(new AsyncCallback(ReceiveCallback), UDPClient);
+        }
+
+        /// <summary>
+        /// Callback for receive.
+        /// </summary>
+        /// <param name="ar">asynchronous call result</param>
+        void ReceiveCallback(IAsyncResult ar)
+        {
+            RaiseEvents(UDPClient.EndReceive(ar, ref EndPoint));
+            BeginReceive();
+        }
+
+        /// <summary>
+        /// Raise events.
+        /// </summary>
+        /// <param name="bytes">bytes array</param>
+        private void RaiseEvents(byte[] bytes)
+        {
+            if (bytes == null)
+            {
+                return;
+            }
+
+            if (OnReceiveBytes != null)
+            {
+                OnReceiveBytes(bytes);
+            }
+
+            if (OnReceiveString != null)
+            {
+                OnReceiveString(Encoding.ASCII.GetString(bytes, 0, bytes.Length));
+            }
+        }
+    }
+}
