b862cf06c616700fd79cb827083bcadb8aaaf942
Joy Developing <joydeveloping@gmail.com>
2014-12-24 18:30:51 +0300

    Lib : SqlConnector, SimplexMethod

---
+++ Lib/Utils/SimplexMethod.cs
@@ -0,0 +1,524 @@
+﻿// Copyright Joy Developing.
+
+using System;
+
+namespace Lib.Utils
+{
+    /// <summary>
+    /// Result.
+    /// </summary>
+    public enum SimplexMethodIterResult
+    {
+        /// <summary>
+        /// Common iteration.
+        /// </summary>
+        Common,
+
+        /// <summary>
+        /// Optimal solution is found.
+        /// </summary>
+        Optimal,
+
+        /// <summary>
+        /// System is unsolvable.
+        /// </summary>
+        Unsolvable
+    }
+
+    /// <summary>
+    /// Simplex method in standard form
+    /// (all conditions are inequalities).
+    /// </summary>
+    public class SimplexMethod
+    {
+        /// <summary>
+        /// Count of variables.
+        /// </summary>
+        private int _N;
+
+        /// <summary>
+        /// Count of variables access.
+        /// </summary>
+        public int N
+        {
+            get
+            {
+                return _N;
+            }
+            private set
+            {
+                _N = value;
+            }
+        }
+
+        /// <summary>
+        /// Count of inequalities (basis size).
+        /// </summary>
+        private int M;
+
+        /// <summary>
+        /// Matrix <c>A</c> of inequalities coefficients.
+        /// </summary>
+        private double[,] A;
+
+        /// <summary>
+        /// Vector <c>B</c> of free members.
+        /// </summary>
+        private double[] B;
+
+        /// <summary>
+        /// Vector <c>C</c> of targer function coefficients.
+        /// </summary>
+        private double[] C;
+
+        /// <summary>
+        /// Vector of basis vectors numbers.
+        /// </summary>
+        private int[] Bas;
+
+        /// <summary>
+        /// Vector <c>Delta</c>.
+        /// </summary>
+        private double[] D;
+
+        /// <summary>
+        /// Value <c>Z</c> of target function.
+        /// </summary>
+        private double Z;
+
+        /// <summary>
+        /// Solution.
+        /// </summary>
+        private double[] X;
+
+        /// <summary>
+        /// Initialize.
+        /// </summary>
+        private void InitT()
+        {
+            A = new double[M, N + M];
+            B = new double[M];
+            C = new double[N + M];
+            Bas = new int[M];
+            D = new double[N + M];
+            X = new double[N];
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="n">Count of variables.</param>
+        /// <param name="m">Basis size.</param>
+        public SimplexMethod(int n, int m)
+        {
+            N = n;
+            M = m;
+            InitT();
+        }
+
+        /// <summary>
+        /// Set coefficient <c>a</c>.
+        /// </summary>
+        /// <param name="i">row number</param>
+        /// <param name="j">column number</param>
+        /// <param name="v">value</param>
+        public void SetA(int i, int j, double v)
+        {
+            A[i, j] = v;
+        }
+
+        /// <summary>
+        /// Set all coefficients <c>a</c>.
+        /// </summary>
+        /// <param name="a">matrix</param>
+        public void SetAllA(double[,] a)
+        {
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    SetA(i, j, a[i, j]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set coefficient <c>b</c>.
+        /// </summary>
+        /// <param name="i">coefficient number</param>
+        /// <param name="v">value</param>
+        public void SetB(int i, double v)
+        {
+            B[i] = v;
+        }
+
+        /// <summary>
+        /// Set all coefficients <c>b</c>.
+        /// </summary>
+        /// <param name="b">vector</param>
+        public void SetAllB(double[] b)
+        {
+            for (int i = 0; i < M; i++)
+            {
+                SetB(i, b[i]);
+            }
+        }
+
+        /// <summary>
+        /// Set coefficient <c>c</c>.
+        /// </summary>
+        /// <param name="j">coefficient number</param>
+        /// <param name="v">value</param>
+        public void SetC(int j, double v)
+        {
+            C[j] = v;
+        }
+
+        /// <summary>
+        /// Set all coefficients <c>c</c>.
+        /// </summary>
+        /// <param name="c">vector</param>
+        public void SetAllC(double[] c)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                SetC(j, c[j]);
+            }
+        }
+
+        /// <summary>
+        /// Get <c>Z</c>.
+        /// </summary>
+        /// <returns>value</returns>
+        public double GetZ()
+        {
+            return Z;
+        }
+
+        /// <summary>
+        /// Get value of result element.
+        /// </summary>
+        /// <param name="i">element number</param>
+        /// <returns>value</returns>
+        public double GetX(int i)
+        {
+            return X[i];
+        }
+
+        /// <summary>
+        /// Set first basis.
+        /// </summary>
+        private void SetFirstBasis()
+        {
+            // Singular mastix of rang M.
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    A[i, j + N] = ((i == j) ? 1.0 : 0.0);
+                }
+            }
+
+            // First basis - additional variables.
+            for (int i = 0; i < M; i++)
+            {
+                int j = i + N;
+                Bas[i] = j;
+                C[j] = 0.0;
+            }
+
+            // First solution - zero.
+            for (int j = 0; j < N; j++)
+            {
+                X[j] = 0.0;
+            }
+        }
+
+        /// <summary>
+        /// Calculate <c>Delta</c>.
+        /// </summary>
+        /// <param name="j">coefficient number</param>
+        private void CalcDelta(int j)
+        {
+            double v = 0.0;
+
+            for (int i = 0; i < M; i++)
+            {
+                v += A[i, j] * C[Bas[i]];
+            }
+
+            D[j] = v - C[j];
+        }
+
+        /// <summary>
+        /// Calculate all coefficients <c>Delta</c>.
+        /// </summary>
+        private void CalcDeltas()
+        {
+            for (int j = 0; j < N + M; j++)
+            {
+                CalcDelta(j);
+            }
+        }
+
+        /// <summary>
+        /// Calculate target function value.
+        /// </summary>
+        private void CalcZ()
+        {
+            double v = 0.0;
+
+            for (int i = 0; i < M; i++)
+            {
+                v += B[i] * C[Bas[i]];
+            }
+
+            Z = v;
+        }
+
+        /// <summary>
+        /// Find lead column.
+        /// </summary>
+        /// <returns>Lead column number or -1, if solution is optimal.</returns>
+        private int FindJ0()
+        {
+            int j0 = -1;
+            double min_d = 0.0;
+
+            for (int j = 0; j < N + M; j++)
+            {
+                if (D[j] > 0.0)
+                {
+                    continue;
+                }
+
+                if ((j0 < 0) || (D[j] < min_d))
+                {
+                    min_d = D[j];
+                    j0 = j;
+                }
+            }
+
+            if (min_d >= 0.0)
+            {
+                // Solution is optimal.
+                return -1;
+            }
+
+            return j0;
+        }
+
+        /// <summary>
+        /// Find lead row.
+        /// </summary>
+        /// <param name="j0">lead column number</param>
+        /// <returns>row number</returns>
+        private int FindI0(int j0)
+        {
+            int i0 = -1;
+            double ba, min_ba = 0.0;
+
+            for (int i = 0; i < M; i++)
+            {
+                if (A[i, j0] <= 0.0)
+                {
+                    // Coefficient must be positive.
+                    continue;
+                }
+
+                ba = B[i] / A[i, j0];
+
+                if ((i0 < 0) || (ba < min_ba))
+                {
+                    min_ba = ba;
+                    i0 = i;
+                }
+            }
+
+            if (i0 < 0)
+            {
+                // System is unsolvable.
+                return -1;
+            }
+
+            return i0;
+        }
+
+        /// <summary>
+        /// Recalc by lead element.
+        /// </summary>
+        /// <param name="i0">lead row number</param>
+        /// <param name="j0">lead column number</param>
+        private void RecalcTable(int i0, int j0)
+        {
+            double a = A[i0, j0];
+
+            // Row i0 normalization.
+            B[i0] /= a;
+            for (int j = 0; j < N + M; j++)
+            {
+                A[i0, j] /= a;
+            }
+
+            // Refresh all rows after this.
+            for (int i = 0; i < M; i++)
+            {
+                if (i == i0)
+                {
+                    continue;
+                }
+
+                double k = A[i, j0];
+
+                B[i] -= B[i0] * k;
+                for (int j = 0; j < N + M; j++)
+                {
+                    A[i, j] -= A[i0, j] * k;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Push vector <c>j0</c> to the basis instead of <c>Bas[i0]</c>.
+        /// </summary>
+        /// <param name="j0">new vector number</param>
+        /// <param name="i0">old vector position</param>
+        private void ChangeBas(int j0, int i0)
+        {
+            Bas[i0] = j0;
+
+            for (int j = 0; j < N; j++)
+            {
+                X[j] = 0.0;
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                if (Bas[i] < N)
+                {
+                    X[Bas[i]] = B[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// One iteration.
+        /// </summary>
+        /// <returns>iteration result</returns>
+        private SimplexMethodIterResult MakeIter()
+        {
+            int i0, j0;
+
+            CalcDeltas();
+
+            j0 = FindJ0();
+
+            if (j0 < 0)
+            {
+                // Solution is optimal.
+                return SimplexMethodIterResult.Optimal;
+            }
+
+            i0 = FindI0(j0);
+
+            if (i0 < 0)
+            {
+                // System is unsolvable.
+                return SimplexMethodIterResult.Unsolvable;
+            }
+
+            RecalcTable(i0, j0);
+            ChangeBas(j0, i0);
+            CalcZ();
+
+            return SimplexMethodIterResult.Common;
+        }
+
+        /// <summary>
+        /// Run simplex-method.
+        /// </summary>
+        /// <returns>calculation result</returns>
+        public SimplexMethodIterResult Run()
+        {
+            SimplexMethodIterResult iter_res;
+
+            SetFirstBasis();
+
+            do
+            {
+                iter_res = MakeIter();
+            } while (iter_res == SimplexMethodIterResult.Common);
+
+            PrintX();
+
+            return iter_res;
+        }
+
+        /// <summary>
+        /// Print simplex table.
+        /// </summary>
+        public void PrintT()
+        {
+            Console.WriteLine("T : ");
+
+            for (int i = 0; i < M; i++)
+            {
+                Console.Write("    {0} {1,5:f} |", Bas[i], B[i]);
+
+                for (int j = 0; j < N + M; j++)
+                {
+                    Console.Write(" {0,5:f} ", A[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.Write("             ");
+
+            for (int j = 0; j < N + M; j++)
+            {
+                Console.Write(" {0,5:f} ", D[j]);
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Print target function value.
+        /// </summary>
+        public void PrintZ()
+        {
+            Console.WriteLine("Z : {0}", Z);
+        }
+
+        /// <summary>
+        /// Print <c>X</c>.
+        /// </summary>
+        public void PrintX()
+        {
+            Console.Write("X : ");
+
+            for (int j = 0; j < N; j++)
+            {
+                Console.Write(" {0} ", X[j]);
+            }
+
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Print basis.
+        /// </summary>
+        public void PrintBas()
+        {
+            Console.Write("Bas : ");
+
+            for (int i = 0; i < M; i++)
+            {
+                Console.Write(" {0} ", Bas[i]);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Lib/Utils/SqlConnector.cs
@@ -0,0 +1,234 @@
+﻿// Copyright Joy Developing.
+
+using System;
+using System.Collections.Generic;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Tools.Utils.SQL
+{
+    /// <summary>
+    /// Class for support interoperating with <c>Microsoft SQL Server 2005</c>.
+    ///
+    /// <b>WARNING!</b>
+    /// Other databases are not supported and never will be.
+    /// </summary>
+    public class SqlConnector
+    {
+        /// <summary>
+        /// Connection.
+        /// </summary>
+        private SqlConnection Connection;
+
+        /// <summary>
+        /// Connection string.
+        /// </summary>
+        public string ConnectionString;
+
+        /// <summary>
+        /// Transaction.
+        /// </summary>
+        private SqlTransaction Transaction;
+
+        /// <summary>
+        /// Data set.
+        /// </summary>
+        private DataSet Set;
+
+        /// <summary>
+        /// Adapters collection.
+        /// </summary>
+        private Dictionary<string, SqlDataAdapter> AdaptersDict;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public SqlConnector()
+        {
+            AdaptersDict = new Dictionary<string, SqlDataAdapter>();
+            Set = new DataSet();
+        }
+
+        /// <summary>
+        /// Connect to database.
+        /// </summary>
+        public void Connect()
+        {
+            Connection = new SqlConnection(ConnectionString);
+            Connection.Open();
+            Transaction = Connection.BeginTransaction();
+        }
+
+        /// <summary>
+        /// Connect to database.
+        /// </summary>
+        /// <param name="connection_string">string</param>
+        public void Connect(string connection_string)
+        {
+            ConnectionString = connection_string;
+            Connect();
+        }
+
+        /// <summary>
+        /// Disconnect.
+        /// </summary>
+        public void Disconnect()
+        {
+            Transaction.Rollback();
+            Connection.Close();
+        }
+
+        /// <summary>
+        /// Transaction commit.
+        /// </summary>
+        public void Commit()
+        {
+            Transaction.Commit();
+            Transaction = Connection.BeginTransaction();
+        }
+
+        /// <summary>
+        /// Transaction rollback.
+        /// </summary>
+        public void Rollback()
+        {
+            Transaction.Rollback();
+            Transaction = Connection.BeginTransaction();
+        }
+
+        /// <summary>
+        /// Scalar query.
+        /// </summary>
+        /// <param name="query">query</param>
+        /// <returns>result</returns>
+        public Object ExecuteScalar(string query)
+        {
+            SqlCommand command = new SqlCommand(query, Connection, Transaction);
+            command.CommandType = CommandType.Text;
+
+            return command.ExecuteScalar();
+        }
+
+        /// <summary>
+        /// Check if has adapter.
+        /// </summary>
+        /// <param name="name">adapter name</param>
+        /// <returns>result</returns>
+        private bool HasAdapter(string name)
+        {
+            return AdaptersDict.ContainsKey(name);
+        }
+
+        /// <summary>
+        /// Add adapter.
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <returns>added adapter</returns>
+        private SqlDataAdapter AddAdapter(string name)
+        {
+            if (!HasAdapter(name))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter();
+                adapter.TableMappings.Add("Table", name);
+                AdaptersDict.Add(name, adapter);
+
+                return adapter;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Delete adapter.
+        /// </summary>
+        /// <param name="name">name</param>
+        public void DeleteAdapter(string name)
+        {
+            if (HasAdapter(name))
+            {
+                AdaptersDict.Remove(name);
+            }
+        }
+
+        /// <summary>
+        /// Get adapter.
+        /// <param name="name">name</param>
+        /// <returns>adapter</returns>
+        /// </summary>
+        private SqlDataAdapter GetAdapter(string name)
+        {
+            SqlDataAdapter adapter;
+
+            if (HasAdapter(name))
+            {
+                AdaptersDict.TryGetValue(name, out adapter);
+            }
+            else
+            {
+                adapter = null;
+            }
+
+            return adapter;
+        }
+
+        /// <summary>
+        /// Set command <c>select</c> for adapter.
+        /// </summary>
+        /// <param name="name">name</param>
+        /// <param name="query">query</param>
+        public void SetSelectCommand(string name, string query)
+        {
+            SqlDataAdapter adapter;
+            SqlCommand command;
+
+            AddAdapter(name);
+            adapter = GetAdapter(name);
+            command = new SqlCommand(query, Connection, Transaction);
+            command.CommandType = CommandType.Text;
+            adapter.SelectCommand = command;
+        }
+
+        /// <summary>
+        /// Fill data set.
+        /// </summary>
+        /// <param name="name">adapter name</param>
+        public void Fill(string name)
+        {
+            SqlDataAdapter adapter = GetAdapter(name);
+
+            if (adapter != null)
+            {
+                adapter.Fill(Set);
+            }
+        }
+
+        /// <summary>
+        /// Link adapter to table.
+        /// </summary>
+        /// <param name="dgv">data grid view</param>
+        /// <param name="name">adapter name</param>
+        public void Link(DataGridView dgv, string name)
+        {
+            dgv.DataSource = Set;
+            dgv.DataMember = name;
+        }
+
+        /// <summary>
+        /// Update changes to database.
+        /// </summary>
+        /// <param name="name">adapter name</param>
+        public void Update(string name)
+        {
+            SqlDataAdapter adapter = GetAdapter(name);
+
+            if (adapter != null)
+            {
+                adapter.Update(Set);
+            }
+        }
+    }
+}
