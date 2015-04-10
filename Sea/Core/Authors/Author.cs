2200f1ac09659fbe47cf9ea039c3c37ab2fa840a
Alexey Rybakov <rybakov.aax@gmail.com>
2015-04-10 13:39:18 +0300

    Sea : Authors

---
+++ Sea/Core/Authors/Author.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Sea.Core.Authors
+{
+    /// <summary>
+    /// Author class.
+    /// </summary>
+    public class Author
+    {
+        /// <summary>
+        /// First name of author.
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Second name of author.
+        /// </summary>
+        public string SecondName { get; set; }
+
+        /// <summary>
+        /// Last Name of author.
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="first_name">first name</param>
+        /// <param name="second_name">second name</param>
+        /// <param name="last_name">last name</param>
+        public Author(string first_name, string second_name, string last_name)
+        {
+            FirstName = first_name;
+            SecondName = second_name;
+            LastName = last_name;
+        }
+
+        /// <summary>
+        /// Constructor by first and last names.
+        /// </summary>
+        /// <param name="first_name">first name</param>
+        /// <param name="last_name">last name</param>
+        public Author(string first_name, string last_name)
+            : this(first_name, "", last_name)
+        {
+        }
+
+        /// <summary>
+        /// Get name.
+        /// </summary>
+        /// <param name="style">style of name print</param>
+        /// <returns>name</returns>
+        public string Name(AuthorNamePrintStyle style)
+        {
+            Debug.Assert((FirstName != "") && (LastName != ""), "unknown author first or last name");
+
+            switch (style)
+            {
+                case AuthorNamePrintStyle.FirstSecondLast:
+
+                    if (SecondName != "")
+                    {
+                        return String.Format("{0} {1} {2}", FirstName, SecondName, LastName);
+                    }
+                    else
+                    {
+                        return Name(AuthorNamePrintStyle.FistLast);
+                    }
+
+                case AuthorNamePrintStyle.FirstSLast:
+
+                    if (SecondName != "")
+                    {
+                        return String.Format("{0} {1}. {2}", FirstName, SecondName[0], LastName);
+                    }
+                    else
+                    {
+                        return Name(AuthorNamePrintStyle.FistLast);
+                    }
+
+                case AuthorNamePrintStyle.FistLast:
+                    return String.Format("{0} {1}", FirstName, LastName);
+
+                case AuthorNamePrintStyle.LastFS:
+
+                    if (SecondName != "")
+                    {
+                        return String.Format("{0} {1}. {2}.", LastName, FirstName[0], SecondName[0]);
+                    }
+                    else
+                    {
+                        return Name(AuthorNamePrintStyle.LastF);
+                    }
+
+                case AuthorNamePrintStyle.LastF:
+                    return String.Format("{0} {1}.", LastName, FirstName[0]);
+
+                default:
+                    Debug.Assert(false, "unknown author name print style");
+                    return "";
+            }
+        }
+    }
+}
---
+++ Sea/Core/Authors/AuthorNamePrintStyle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sea.Core.Authors
+{
+    /// <summary>
+    /// Style of author name printing.
+    /// Example on Michael Joseph Jackson.
+    /// </summary>
+    public enum AuthorNamePrintStyle
+    {
+        /// <summary>
+        /// Michael Joseph Jackson
+        /// </summary>
+        FirstSecondLast,
+
+        /// <summary>
+        /// Michael J. Jackson
+        /// </summary>
+        FirstSLast,
+
+        /// <summary>
+        /// Michael Jackson
+        /// </summary>
+        FistLast,
+
+        /// <summary>
+        /// Jackson M. J.
+        /// </summary>
+        LastFS,
+
+        /// <summary>
+        /// Jackson M.
+        /// </summary>
+        LastF
+    }
+}
