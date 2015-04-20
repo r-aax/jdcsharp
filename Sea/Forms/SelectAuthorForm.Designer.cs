a8c77f7ec69a91ee3526dfc086575b4ed78aa525
Alexey Rybakov <rybakov.aax@gmail.com>
2015-04-20 16:46:56 +0300

    Sea : select author form ; initial

---
+++ Sea/Forms/SelectAuthorForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Sea.Forms
+{
+    partial class SelectAuthorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AcceptB = new System.Windows.Forms.Button();
+            this.CancelB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(480, 446);
+            this.listBox1.TabIndex = 0;
+            // 
+            // AcceptB
+            // 
+            this.AcceptB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AcceptB.ForeColor = System.Drawing.Color.OliveDrab;
+            this.AcceptB.Location = new System.Drawing.Point(177, 464);
+            this.AcceptB.Name = "AcceptB";
+            this.AcceptB.Size = new System.Drawing.Size(75, 23);
+            this.AcceptB.TabIndex = 1;
+            this.AcceptB.Text = "Accept";
+            this.AcceptB.UseVisualStyleBackColor = true;
+            this.AcceptB.Click += new System.EventHandler(this.AcceptB_Click);
+            // 
+            // CancelB
+            // 
+            this.CancelB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CancelB.ForeColor = System.Drawing.Color.IndianRed;
+            this.CancelB.Location = new System.Drawing.Point(258, 464);
+            this.CancelB.Name = "CancelB";
+            this.CancelB.Size = new System.Drawing.Size(75, 23);
+            this.CancelB.TabIndex = 2;
+            this.CancelB.Text = "Cancel";
+            this.CancelB.UseVisualStyleBackColor = true;
+            this.CancelB.Click += new System.EventHandler(this.CancelB_Click);
+            // 
+            // SelectAuthorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 495);
+            this.Controls.Add(this.CancelB);
+            this.Controls.Add(this.AcceptB);
+            this.Controls.Add(this.listBox1);
+            this.MaximumSize = new System.Drawing.Size(520, 533);
+            this.MinimumSize = new System.Drawing.Size(520, 533);
+            this.Name = "SelectAuthorForm";
+            this.Text = "Author selection";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button AcceptB;
+        private System.Windows.Forms.Button CancelB;
+    }
+}
---
+++ Sea/Forms/SelectAuthorForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sea.Forms
+{
+    /// <summary>
+    /// Select author form.
+    /// </summary>
+    public partial class SelectAuthorForm : Form
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public SelectAuthorForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Accept button click.
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">parameters</param>
+        private void AcceptB_Click(object sender, EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Cancel button click.
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">parameters</param>
+        private void CancelB_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
