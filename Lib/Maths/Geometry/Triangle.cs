cbe5086aa094bdcda3171406eff00109e8e9653b
Alexey Rybakov <rybakov.aax@gmail.com>
2015-08-05 16:49:14 +0300

    Geometry : triangle angles calculations

--- Lib/Maths/Geometry/Triangle.cs
+++ Lib/Maths/Geometry/Triangle.cs
@@ -64,40 +64,73 @@ namespace Lib.Maths.Geometry
         }
 
         /// <summary>
-        /// Altitude from A to BC.
+        /// Angle A.
         /// </summary>
-        public double AltitudeA
+        public double AngleA
         {
             get
             {
-                // Square = 0.5 * BC * AltitudeA.
-                // AltitudeA = Square / (0.5 * BC) = 2 * Square / BC.
+                return Math.Acos((AB * AB + AC * AC - BC * BC) / (2.0 * AB * AC));
+            }
+        }
+
+        /// <summary>
+        /// Angle B.
+        /// </summary>
+        public double AngleB
+        {
+            get
+            {
+                return Math.Acos((AB * AB + BC * BC - AC * AC) / (2.0 * AB * BC));
+            }
+        }
+
+        /// <summary>
+        /// Angle C.
+        /// </summary>
+        public double AngleC
+        {
+            get
+            {
+                return Math.Acos((BC * BC + AC * AC - AB * AB) / (2.0 * BC * AC));
+            }
+        }
+
+        /// <summary>
+        /// Height from A to BC.
+        /// </summary>
+        public double HeightA
+        {
+            get
+            {
+                // Square = 0.5 * BC * HeightA.
+                // HeightA = Square / (0.5 * BC) = 2 * Square / BC.
                 return 2.0 * Square / BC;
             }
         }
 
         /// <summary>
-        /// Altitude from B to AC.
+        /// Height from B to AC.
         /// </summary>
-        public double AltitudeB
+        public double HeightB
         {
             get
             {
-                // Square = 0.5 * AC * AltitudeB.
-                // AltitudeB = Square / (0.5 * AC) = 2 * Square / AC.
+                // Square = 0.5 * AC * HeightB.
+                // HeightB = Square / (0.5 * AC) = 2 * Square / AC.
                 return 2.0 * Square / AC;
             }
         }
 
         /// <summary>
-        ///  Altitute from C to AB.
+        ///  Height from C to AB.
         /// </summary>
-        public double AltitudeC
+        public double HeightC
         {
             get
             {
-                // Square = 0.5 * AB * AltitudeC.
-                // AltitudeC = Square / (0.5 * AB) = 2 * Square / AB.
+                // Square = 0.5 * AB * HeightC.
+                // HeightC = Square / (0.5 * AB) = 2 * Square / AB.
                 return 2.0 * Square / AB;
             }
         }
