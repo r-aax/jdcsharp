1e1074e430f892634e0803646308ad80f81547e2
Alexey Rybakov <rybakov@jscc.ru>
2016-08-15 15:11:22 +0300

    DrawBox : initialization

---
+++ DrawBox/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrawBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Click on Draw PlanOMP menu item.
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">parameters</param>
+        private void DrawPlanOMP_MI_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        /// <summary>
+        /// Click on Picture Save menu item.
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">parameters</param>
+        private void PictureSaveMI_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+    }
+}
