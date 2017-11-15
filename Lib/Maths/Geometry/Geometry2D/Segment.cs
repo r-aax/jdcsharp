f9c2a33697c858c23333f7dac6a953a86168af86
Alexey Rybakov <rybakov.aax@gmail.com>
2017-11-15 17:51:20 +0300

    Lib : 2D segment

---
+++ Lib/Maths/Geometry/Geometry2D/Segment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Maths.Geometry.Geometry2D
+{
+    /// <summary>
+    /// Segment.
+    /// </summary>
+    public class Segment
+    {
+        /// <summary>
+        /// First point.
+        /// </summary>
+        public Point A;
+
+        /// <summary>
+        /// Second point.
+        /// </summary>
+        public Point B;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="a">first point</param>
+        /// <param name="b">second point</param>
+        public Segment(Point a, Point b)
+        {
+            A = a;
+            B = b;
+        }
+    }
+}
