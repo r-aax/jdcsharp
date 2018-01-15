68493f4894acaa2de8b32b28e259bc1330508cd1
Alexey Rybakov <rybakov.aax@gmail.com>
2018-01-15 12:50:55 +0300

    Lib : NVector additional functions

--- Lib/Maths/NVector.cs
+++ Lib/Maths/NVector.cs
@@ -9,7 +9,7 @@ namespace Lib.Maths
     /// <summary>
     /// Vector with components count greater than three.
     /// </summary>
-    public class NVector
+    public class NVector : ICloneable
     {
         /// <summary>
         /// Array of elements.
@@ -122,6 +122,26 @@ namespace Lib.Maths
             }
 
             return String.Format("({0})", str);
+        }
+
+        /// <summary>
+        /// Clone.
+        /// </summary>
+        /// <returns>clone</returns>
+        public virtual object Clone()
+        {
+            return new NVector(this);
+        }
+
+        /// <summary>
+        /// Copy of vector.
+        /// </summary>
+        public NVector Copy
+        {
+            get
+            {
+                return Clone() as NVector;
+            }
         }
 
         /// <summary>
@@ -260,6 +280,120 @@ namespace Lib.Maths
         public static NVector operator /(NVector a, double k)
         {
             return a * (1.0 / k);
+        }
+
+        /// <summary>
+        /// Module of vector.
+        /// </summary>
+        public NVector Abs
+        {
+            get
+            {
+                NVector v = Copy;
+
+                for (int i = 0; i < Size; i++)
+                {
+                    E[i] = Math.Abs(E[i]);
+                }
+
+                return v;
+            }
+        }
+
+        /// <summary>
+        /// Index of minimum element.
+        /// </summary>
+        public int IndexOfMinElement
+        {
+            get
+            {
+                if (Size == 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    int ind = 0;
+
+                    for (int i = 1; i < Size; i++)
+                    {
+                        if (E[i] < E[ind])
+                        {
+                            ind = i;
+                        }
+                    }
+
+                    return ind;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Index of maximum element.
+        /// </summary>
+        public int IndexOfMaxElement
+        {
+            get
+            {
+                if (Size == 0)
+                {
+                    return -1;
+                }
+                else
+                {
+                    int ind = 0;
+
+                    for (int i = 1; i < Size; i++)
+                    {
+                        if (E[i] > E[ind])
+                        {
+                            ind = i;
+                        }
+                    }
+
+                    return ind;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Index of minimum module element.
+        /// </summary>
+        public int IndexOfMinModElement
+        {
+            get
+            {
+                return Abs.IndexOfMinElement;
+            }
+        }
+
+        /// <summary>
+        /// Index of minimum module element.
+        /// </summary>
+        public int IndexOfMaxModElement
+        {
+            get
+            {
+                return Abs.IndexOfMaxElement;
+            }
+        }
+
+        /// <summary>
+        /// Round vecrtor.
+        /// </summary>
+        public NVector Round
+        {
+            get
+            {
+                NVector v = Copy;
+
+                for (int i = 0; i < Size; i++)
+                {
+                    E[i] = Math.Round(E[i]);
+                }
+
+                return v;
+            }
         }
     }
 }
