41f324672e5deef0a10ca764905873f19cf33994
Alexey Rybakov <rybakov.aax@gmail.com>
2018-02-27 13:57:13 +0300

    GridMaster console version. Initial commit.

---
+++ GridMasterConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridMasterConsole
+{
+    /// <summary>
+    /// Console version of GridMaster.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Main function.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
